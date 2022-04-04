972808373b3369df40a3097874e83d17f8c971ca
Manuel La Placa <laplacamanuel0@gmail.com>
2022-04-04 11:36:12 +0100

    more questions added

---
+++ data/questions.js
@@ -0,0 +1,128 @@
+export const questions = [
+    {
+        question: 'What is the capital city of Germany?',
+        answers: [
+            {
+                answer: 'Berlin',
+                isCorrect: true
+            },
+            {
+                answer: 'Munich',
+                isCorrect: false
+            },
+            {
+                answer: 'Frankfurt',
+                isCorrect: false
+            },
+            {
+                answer: 'Paris',
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        question: 'In what year the French revolution happened?',
+        answers: [
+            {
+                answer: '1789',
+                isCorrect: true
+            },
+            {
+                answer: '1986',
+                isCorrect: false
+            },
+            {
+                answer: '1600',
+                isCorrect: false
+            },
+            {
+                answer: '2016',
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        question: 'What animal is not a mammal?',
+        answers: [
+            {
+                answer: 'Dolphin',
+                isCorrect: false
+            },
+            {
+                answer: 'Elephant',
+                isCorrect: false
+            },
+            {
+                answer: 'Crocodile',
+                isCorrect: true
+            },
+            {
+                answer: 'Armadillo',
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        question: 'Who won the Football world championship in 2006?',
+        answers: [
+            {
+                answer: 'France',
+                isCorrect: false
+            },
+            {
+                answer: 'Brasil',
+                isCorrect: false
+            },
+            {
+                answer: 'Germany',
+                isCorrect: false
+            },
+            {
+                answer: 'Italy',
+                isCorrect: true
+            }
+        ]
+    },
+    {
+        question: 'Who first claimed the heart was rounded?',
+        answers: [
+            {
+                answer: 'Cristoforo Colombo',
+                isCorrect: false
+            },
+            {
+                answer: 'Galileo Galilei',
+                isCorrect: true
+            },
+            {
+                answer: 'Marco Polo',
+                isCorrect: false
+            },
+            {
+                answer: 'George Orwell',
+                isCorrect: false
+            }
+        ]
+    },
+    {
+        question: 'What is the fastest animal on land?',
+        answers: [
+            {
+                answer: 'Tiger',
+                isCorrect: false
+            },
+            {
+                answer: 'Oistrich',
+                isCorrect: false
+            },
+            {
+                answer: 'Chetaah',
+                isCorrect: true
+            },
+            {
+                answer: 'Dog',
+                isCorrect: false
+            }
+        ]
+    },
+]
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import { questions } from './data/questions.js';
+
+console.log(questions)
+
 const homeBtn = document.querySelector('.home__btn');
 console.log(homeBtn);
 const userScoreQs = document.querySelector('.user__score');
@@ -6,17 +10,181 @@ const currentScoreQs = document.querySelector('.current__score');
 console.log(currentScoreQs);
 const imgDisplayed = document.querySelector('.question__image');
 console.log(imgDisplayed);
-const questionsDisplayed = document.querySelector('.title__question');
-console.log(questionsDisplayed);
-const answerBtns = document.querySelectorAll('.btn');
-console.log(answerBtns);
 const countDownAnswer = document.querySelectorAll('#countdown');
 console.log(countDownAnswer);
-const nextQuesitonBtn = document.querySelectorAll('#next-question');
-console.log(nextQuesitonBtn);
+const nextBtn = document.querySelector('#next-question');
+const quiz = document.querySelector('.quiz');
+console.log(quiz);
+// const questionsDisplayed = document.querySelector('.title__question');
+// console.log(questionsDisplayed);
+// const answerBtns = document.querySelectorAll('.btn');
+// console.log(answerBtns);
 
 let homeGame = "";
-let score = 0;
+let user = 0;
 let quizImg = "";
 let quizQuesiton = "";
-let nextBtn = "";
+let currentQuestion = 0;
+
+
+// CODE HERE TO MAKE ANSWER BTNS FUNCTIONAL
+
+const renderFirstQuestion = (initialIndex) => {
+    console.log(initialIndex) 
+    let html = `<section class="quiz__container">
+    <div class="question__image">
+         <img src="./images/trulli.jpeg" alt="er-trullo" />
+    </div>
+    <h2 class="title__question">${questions[initialIndex].question}</h2>
+    <div class="answers__btns">
+    <button class="btn btn--1" data-correct=${questions[initialIndex].answers[0].isCorrect}>${questions[initialIndex].answers[0].answer}</button>
+    <button class="btn btn--2">${questions[initialIndex].answers[1].answer}</button>
+    <button class="btn btn--3">${questions[initialIndex].answers[2].answer}</button>
+    <button class="btn btn--4">${questions[initialIndex].answers[3].answer}</button>
+    </div>
+    </section>`;
+    
+    quiz.innerHTML += html;
+
+}
+
+renderFirstQuestion(0)
+
+const answerBtns = document.querySelectorAll('.btn');
+
+// const renderNextQuestion = (updatedIndex) => {
+//     quiz.innerHTML = '';
+//     let html = `<section class="quiz__container">
+//     <div class="question__image">
+//          <img src="./images/trulli.jpeg" alt="er-trullo" />
+//     </div>
+//     <h2 class="title__question">${questions[updatedIndex].question}</h2>
+//     <div class="answers__btns">
+//     <button class="btn btn--1">${questions[updatedIndex].answers[0].answer}</button>
+//     <button class="btn btn--2">${questions[updatedIndex].answers[1].answer}</button>
+//     <button class="btn btn--3">${questions[updatedIndex].answers[2].answer}</button>
+//     <button class="btn btn--4">${questions[updatedIndex].answers[3].answer}</button>
+//     </div>
+//     </section>`;
+//     return quiz.innerHTML += html;
+// }
+
+
+answerBtns.forEach((answerBtn) => {
+    console.log(answerBtn);
+    answerBtn.addEventListener('click', () => {
+        console.log('button clicked')
+        validateAnswer(answerBtn)
+    });
+});
+//WHY IS ANSWERBTNS FUNCTION PLACED ABOVE VALIDATE ANSWER FUNCITON?
+const validateAnswer = (answerClicked) => {
+    console.log(answerClicked)
+    let answerValidation = answerClicked.getAttribute('data-correct');
+    console.log(answerValidation)
+    if(answerValidation) {
+        console.log('if statement ran')
+        answerClicked.classList.add('correct');
+        return console.log('Congratulations! That is the correct answer');
+    } else {
+        return console.log('Sorry that is not correct')
+    }
+
+}
+
+// const scoreUp = () => {
+//     if (answerClicked === isCorrect) {
+//         return  userScoreQs.innerHTML += 1 
+//     } else if (answerClicked === false){
+//         return currentQuestion.innerHTML += 1
+//     }
+// }
+
+userScoreQs.forEach (() => {
+    if (answerClicked === isCorrect) {
+                return  userScoreQs.innerHTML += 1 
+            } else if (answerClicked === false){
+                return currentQuestion.innerHTML += 1
+            } 
+})
+
+// currentScoreQs.forEach (() => {
+// validateAnswer += 1
+// })
+
+// const scoreUpdate = validateAnswer  => {
+//     scoreUpdate = currentScoreQs.event.target.value;
+//     scoreUpdate = userScoreQs.event.target.value;
+//     if (scoreUpdate === validateAnswer) {
+//         return userScoreQs += 1; 
+//     } else {
+//         return currentQuestion += 1
+//     }
+// }
+
+
+
+// userScoreQs.forEach((userScoreQ) => {
+//     userScoreQs.addEventListener('change', (event) => {
+//         score = event.target.innerText;
+//     })    
+//     if (answerClicked === true) {
+//             return userScoreQs.innerHTML += 1
+//         } else {
+//             return ;
+//         }
+// });
+
+
+// When we click on an answer we want to validate the answer to see  if its correct/incorrect. 
+// Then we want to update the Score counter after the answer has been validated.
+// If its correct - you want to update both the userScore (left hand side) and the questionNumber (right hand side)
+// Else if its incorrect - you only want to update the questionNumber
+
+// Q's validation
+
+// userScoreQs.forEach() => {
+// userScoreQs.addEventListener ('change', event) => {
+//     userScoreQs.innerHTML = answerBtns
+//}
+//}
+
+// console.log(`userScoreQs`, userScoreQs);
+
+
+
+
+//MAKE A FUNCTION THAT CHANGE Q'S WHEN NEXT BUTTON IS PRESSED
+
+// MAKE A FUNCTION TO CHANGE THE PICTURE WHEN CHANGED THE Q'S
+
+// nextBtn.addEventListener('click', () => {
+//     renderNextQuestion(currentQuestion++)
+// })
+
+//MAKE A FUNCTION FOR THE TIME-LIMIT
+
+// const renderHTML = `<section class="quiz__container">
+// <div class="question__image">
+//     <img src="./images/trulli.jpeg" alt="er-trullo" />
+// </div>
+// <h2 class="title__question">
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+// sapiente hic minima cumque illum quisquam nisi odio qui deserun
+// </h2>
+// <div class="answers__btns">
+// <button class="btn btn--1">1</button>
+// <button class="btn btn--2">2</button>
+// <button class="btn btn--3">3</button>
+// <button class="btn btn--4">4</button>
+// </div>
+// </section>`;
+// console.log(quiz, renderHTML);
+// quiz.innerHTML += renderHTML;
+//MAKE A FUNCTION TO SWITCH THE BACKGORUND COLOR. GREEN IF IT IS THE RIGHT ANSWER, RED IF IT'S THE WRONG 
+
+
+//MAKE A FUNTION TO MAKE THE USER SCORE DYNAMIC. 
+
+
+// next work on the user score (partly will be inside validation function )
